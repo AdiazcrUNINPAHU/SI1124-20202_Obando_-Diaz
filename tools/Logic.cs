55f1eaf0536fabce6afcdee7022c9be5ac002270
AdiazcrUNINPAHU <69880978+AdiazcrUNINPAHU@users.noreply.github.com>
2020-11-30 18:14:35 -0500

    Add files via upload

---
+++ tools/Logic.cs
@@ -0,0 +1,52 @@
+﻿using mercanciaBarco.objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mercanciaBarco.tools
+{
+    public static class Logic
+    {
+        public static void loadTruck()
+        {
+            //Max por cada camion
+            double pesoSoportado = 0.0;
+            double volSoportado = 0.0;
+            
+            foreach (var truck in Program.camiones)
+            {
+                //Se crea lista de paquetes
+                List<Paquete> paq = new List<Paquete>();
+                foreach (var package in Program.paquetes)
+                {
+                    //validacion de peso y volumen maximo segun cada camion
+                    if (((package._peso + pesoSoportado) <= truck._peso) && ((package._volumen + volSoportado) <= truck._volumen))
+                    {
+                        //validacion si aun no se ha cargado el paquete en algun camion
+                        if (!package._inTruck)
+                        {
+                            //Incrementa valores max soportados por camion
+                            pesoSoportado += package._peso;
+                            volSoportado += package._volumen;
+
+                            paq.Add(package);//agrega a lista temporal
+                            package._inTruck = true;
+
+                        }
+                    }
+                    else
+                    {
+                        //Busca en la lista principal el camion que se recorre y agrega la lista temporal de paquetes
+                        Program.camiones.Find(x => x._id == truck._id)._paquetes = paq;
+                    }
+                }
+                //reseteo de variables para cada camion
+                pesoSoportado = 0.0;
+                volSoportado = 0.0;
+                truck._enDespacho = true;
+                Console.WriteLine("Camion con ID:"+truck._id+", salio a reparto");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ tools/UseFiles.cs
@@ -0,0 +1,62 @@
+﻿using mercanciaBarco.objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mercanciaBarco.tools
+{
+    public static class useFiles
+    {
+        public static void createObjects()
+        {
+            string[] objects;
+            bool val = true;//valida para evitar cabeceras de archivos txt
+
+            objects = System.IO.File.ReadAllLines("Camiones.txt");//Lee archivos, almacenado en ...\mercanciaBarco\mercanciaBarco\bin\Debug\netcoreapp3.1
+            foreach (string line in objects)
+            {
+                //evita guardar cabecera en objetos
+                if (val)
+                {
+                    val = false;
+                }
+                else
+                {
+                    //crea objetos
+                    Camion cm = new Camion();
+                    //divide cada linea de ID, peso y volumen
+                    string[] ln = line.Split(';');
+                    //asigna
+                    cm._id = Convert.ToInt16(ln[0]);
+                    cm._volumen = Convert.ToDouble(ln[1]);
+                    cm._peso = Convert.ToDouble(ln[2]);
+                    //agrega a listado principal
+                    Program.camiones.Add(cm);
+                }
+            }
+            val = true;
+
+            objects = System.IO.File.ReadAllLines("Paquetes.txt");
+            foreach (string line in objects)
+            {
+                if (val)
+                {
+                    val = false;
+                }
+                else
+                {
+                    Paquete pq = new Paquete();
+                    string[] ln = line.Split(';');
+                    pq._id = Convert.ToInt32(ln[0]);
+                    pq._volumen = Convert.ToDouble(ln[1]);
+                    pq._peso = Convert.ToDouble(ln[2]);
+
+                    Program.paquetes.Add(pq);
+                }
+            }
+        }
+
+
+
+    }
+}
